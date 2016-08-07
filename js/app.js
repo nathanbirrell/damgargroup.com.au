732412baeafebb867aecc0be00a4890ebf1a228d
Nathan Birrell <nathanbirrell@gmail.com>
2016-08-07 13:55:24 +1000

    logo scroll animations for mobile

--- js/app.js
+++ js/app.js
@@ -7,4 +7,14 @@
 
 Foundation.OffCanvas.defaults.forceTop = false;
 
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 62){
+    $('.sticky').removeClass('expanded');
+    $('.mobile-logo').attr('src', $('#sticky-logo-swap').attr('src'));
+  } else {
+    $('.sticky').addClass('expanded');
+    $('.mobile-logo').attr('src', $('#expanded-logo-swap').attr('src'));
+  }
+});
+
 $(document).foundation();
