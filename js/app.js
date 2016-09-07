7e565f6f63c21ec96fd966ea3b94cea5bb2b23c5
Nathan Birrell <nathanbirrell@gmail.com>
2016-09-07 11:32:01 +1000

    first pass on material design click for CTA buttons

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@
 //   $(element).height(width);
 // });
 
+/**
+ * Mobile sticky menu
+ */
+
 Foundation.OffCanvas.defaults.forceTop = false;
 
 $(window).scroll(function() {
@@ -16,5 +20,36 @@ $(window).scroll(function() {
     $('.mobile-logo').attr('src', $('#expanded-logo-swap').attr('src'));
   }
 });
+
+/**
+ * Material design click for CTA buttons
+ */
+
+ var element, circle, d, x, y;
+ $(".cta-wrap .cta").click(function(e){
+
+ 	element = $(this);
+
+ 	if(element.find(".circle").length == 0)
+ 		element.prepend("<span class='circle'></span>");
+
+ 	circle = element.find(".circle");
+ 	circle.removeClass("animate");
+
+ 	if(!circle.height() && !circle.width())
+   {
+ 		d = Math.max(element.outerWidth(), element.outerHeight());
+ 		circle.css({height: d, width: d});
+ 	}
+
+ 	x = e.pageX - element.offset().left - circle.width()/2;
+ 	y = e.pageY - element.offset().top - circle.height()/2;
+
+ 	circle.css({top: y+'px', left: x+'px'}).addClass("animate");
+});
+
+/**
+ * Initialise Foundation
+ */
 
 $(document).foundation();
