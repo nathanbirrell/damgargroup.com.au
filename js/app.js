100409c7baf7242e419ddca01f10597b5fad4956
Nathan Birrell <nathanbirrell@gmail.com>
2016-09-07 12:10:04 +1000

    material design js refactor

--- js/app.js
+++ js/app.js
@@ -25,30 +25,33 @@ $(window).scroll(function() {
  * Material design click for CTA buttons
  */
 
-var materialDesignObjects = ['.cta-wrap .cta','.project-thumb-wrapper .thumbnail'];
+var materialDesignObjects = [
+  '.cta-wrap .cta',
+  '.project-thumb-wrapper .thumbnail'
+];
 
 $(materialDesignObjects).each(function(index, object) {
   $(object).click(function(e){
-    var element, circle, d, x, y;
+    var element, circle, diameter, clickCoordinatesX, clickCoordingatesY;
 
     element = $(this);
 
-    if(element.find(".circle").length == 0)
-    	element.prepend("<span class='circle'></span>");
-
-    circle = element.find(".circle");
-    circle.removeClass("animate");
-
-    if(!circle.height() && !circle.width())
-    {
-    	d = Math.max(element.outerWidth(), element.outerHeight());
-    	circle.css({height: d, width: d});
+    if (element.find(".circle").length == 0) {
+      element.prepend("<span class='circle'></span>");
     }
 
-    x = e.pageX - element.offset().left - circle.width()/2;
-    y = e.pageY - element.offset().top - circle.height()/2;
+    circle = element.find(".circle");
+    circle.removeClass('animate');
 
-    circle.css({top: y+'px', left: x+'px'}).addClass("animate");
+    if (!circle.height() && !circle.width()) {
+    	diameter = Math.max(element.outerWidth(), element.outerHeight());
+    	circle.css({height: diameter, width: diameter});
+    }
+
+    clickCoordinatesX = e.pageX - element.offset().left - circle.width()/2;
+    clickCoordingatesY = e.pageY - element.offset().top - circle.height()/2;
+
+    circle.css({top: clickCoordingatesY+'px', left: clickCoordinatesX+'px'}).addClass('animate');
   });
 });
 
