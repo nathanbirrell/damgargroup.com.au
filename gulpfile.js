372e6fd4d2b52e91c173a36194058dac75903b77
Nathan Birrell <nathanbirrell@gmail.com>
2016-12-20 16:22:19 +1100

    Release

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+
+var path = require('path')
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
 
 // FILE PATH CONFIG
 var jsFiles = [
@@ -26,23 +29,38 @@ var sassPaths = [
   'bower_components/photoswipe/dist'
 ];
 
-gulp.task('build-and-serve-jekyll', shell.task(['bundle install; exec jekyll build --watch']));
+gulp.task('html', ['jekyll'], function() {
+    // --> Minhtml
+    gulp.src([
+            path.join('_site/', '*.html'),
+            path.join('_site/', '*/*/*.html'),
+            path.join('_site/', '*/*/*/*.html')
+        ])
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('_site/'))
+        .pipe(browserSync.reload({
+            stream: true,
+            once: true
+        }));
+});
+
+gulp.task('jekyll', [], function (gulpCallBack){
+    var spawn = require('child_process').spawn;
+    // After build: cleanup HTML
+    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+    });
+});
 
 gulp.task('build-jekyll', shell.task(['bundle install; bundle exec jekyll build --config _config.yml,_config-prod.yml']));
 
-gulp.task('serve', function () {
-  browserSync.init({
-    server: {
-      baseDir: '_site/'
-    },
-    injectChanges: true
-  });
+gulp.task('serve-jekyll', shell.task(['bundle exec jekyll serve --config _config.yml']));
 
-  gulp.watch('_site/**/*.html').on('change', browserSync.reload);
-  gulp.watch('_site/**/*.js').on('change', browserSync.reload);
-});
-
-gulp.task('sass', function() {
+gulp.task('build-sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths
@@ -56,20 +74,41 @@ gulp.task('sass', function() {
 });
 
 gulp.task('build-js', function() {
-    return gulp.src(jsFiles)
-      // concat
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest(jsDest))
-      // uglify
-      .pipe(rename('scripts.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(jsDest));
+  return gulp.src(jsFiles)
+    // concat
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(jsDest))
+    // uglify
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest))
+    .pipe(browserSync.reload({stream:true, once: true}));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task('serve', function() {
+  browserSync.init(null, {
+      server: {
+          baseDir: "./" + '_site/',
+          injectChanges: true
+      },
+      // logFileChanges: true,
+      // logLevel: 'debug',
+      open: true        // Toggle to automatically open page when starting.
+  });
+
+  gulp.watch('scss/**/*.scss', ['build-sass']);
+
   gulp.watch(jsFiles, ['build-js']);
+
+  // images are built as part of the Jekyll process
+  gulp.watch('img/**/*', ['html']);
+  gulp.watch('project-photos/**/*', ['html']);
+
+  gulp.watch(['_config.yml'], ['html']);
+  gulp.watch(['_posts/**/*.+(md|markdown|MD)'], ['html']);
+  gulp.watch(['**/*.+(html|md|markdown|MD)', '!_site/**/*.*'], ['html']);
 });
 
-gulp.task('default', ['build-and-serve-jekyll','sass','build-js','watch','serve']);
-gulp.task('build', ['build-jekyll','sass','build-js']);
+gulp.task('default', ['html','build-sass','build-js','serve', 'serve-jekyll']);
+
+gulp.task('build', ['build-jekyll','build-sass','build-js']);
